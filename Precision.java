480d15839b6d96a792e0727a93c27551a1dc3af7
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2023-03-27 23:20:56 +0600

    findout explicit precision of a value

---
+++ Precision.java
@@ -0,0 +1,8 @@
+public class Precision {
+    public static void main(String[] args) {
+        double val = 9.0;
+        System.out.println("Precision " + String.format("%.10f", val));
+        double m = 99;
+        System.out.println(String.format("%.10f", m)); //floating point "%.10f" 
+    }
+}
