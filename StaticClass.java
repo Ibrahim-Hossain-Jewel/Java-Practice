081f38f47d6ee7ba1123c8d6f9f0ebae430f6947
ibrahim hossain <ibrahimhossaincse@gmail.com>
2023-04-20 11:04:49 +0600

    nested class method invoke

--- StaticClass.java
+++ StaticClass.java
@@ -3,15 +3,27 @@ public class StaticClass {
     public static String name = "7 class type ";
     public static class MyStatic{
         public void show(){
-            System.out.println("Non-static method.");
+            System.out.println("static class method.");
             System.out.println(name);
         }
         public static void jewel(){
             System.out.println("Jewel");
         }
     }
+    public class NonStatic {
+        public void jewel(){
+            System.out.println("Hi jewel from non static method");
+        }
+        public void show(){ 				//inherits static members from class MyStatic.show() and calls non-static members from class NonStatic.
+            System.out.println("Non-static method.");
+        }
+        
+    }
     public static void main(String[] args){
-        //unable to create static class instance.
+        StaticClass obj = new StaticClass();
+        StaticClass.NonStatic nonStatic = obj.new NonStatic(); //Creating the nested class
+        nonStatic.jewel();
+        //Unable to create static class instance.
         StaticClass.MyStatic ob = new StaticClass.MyStatic();
         ob.show();
         ob.jewel();
