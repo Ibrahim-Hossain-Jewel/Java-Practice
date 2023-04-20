b7ed4163a167659daa1758093c0dd34c13d378a9
ibrahim hossain <ibrahimhossaincse@gmail.com>
2023-04-20 12:14:33 +0600

    final class can not be extends

--- FinalClass.java
+++ FinalClass.java
@@ -1,5 +1,18 @@
+final class student{
+    final class m {
+        void x (){
+            System.out.println("Hi m");
+        }
+    }    
+}
+class another extends student{
+    //final class can not be extends
+}
 public class FinalClass{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Initializing the finalClass");
+        student n = new student();
+        student.m obj = n.new m();
+        obj.x();
     }
 }
