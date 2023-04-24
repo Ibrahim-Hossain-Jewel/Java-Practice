00a6572864d4bd7636de001756ccd3a47eca11e7
ibrahim hossain <ibrahimhossaincse@gmail.com>
2023-04-24 21:27:56 +0600

    read value dynamically from a existing file

--- FileRead.java
+++ FileRead.java
@@ -2,11 +2,19 @@ import java.io.*;
 public class FileRead {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         FileInputStream fis = new FileInputStream("abc.txt");
-        int data1 = fis.read();
-        int data2 = fis.read();
-        int data3 = fis.read();
-        System.out.println("Data1 : " + (char)data1);
-        System.out.println("Data2 : " + (char) data2);
-        System.out.println("Data3 : " + (char)data3);
+        // Without dynamic read file data.
+        // int data1 = fis.read();
+        // int data2 = fis.read();
+        // int data3 = fis.read();
+        // System.out.println("Data1 : " + (char)data1);
+        // System.out.println("Data2 : " + (char) data2);
+        // System.out.println("Data3 : " + data3); 
+        // FileInputStream returns byte by byte data.
+        System.out.println("Dynamic file data read");
+        int all;
+        while((all = fis.read()) != -1){ //You can assign value with while loop.
+            System.out.println(all);
+        }
+        fis.close();
     }
 }
