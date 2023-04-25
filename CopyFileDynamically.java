e91d5e066ce700538d538c7f23b326332649542f
ibrahim hossain <ibrahimhossaincse@gmail.com>
2023-04-25 07:40:15 +0600

    provide user input to copy a file dynamically

--- CopyFileDynamically.java
+++ CopyFileDynamically.java
@@ -2,11 +2,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CopyFileDynamically {
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        FileInputStream fis = new FileInputStream(args[0]); //Input exsiting file.
-        FileOutputStream fos = new FileOutputStream(args[1]);
+        Scanner scnr = new Scanner(System.in);
+        String fileInput = scnr.nextLine();
+        Scanner scnr2 = new Scanner(System.in);
+        String fileDestination = scnr2.nextLine();
+        FileInputStream fis = new FileInputStream(fileInput); //Input exsiting file.
+        FileOutputStream fos = new FileOutputStream(fileDestination);
         int data;
         while((data = fis.read()) != -1){
             fos.write((char)data);
